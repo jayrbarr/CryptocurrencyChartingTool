e2a3f87a25f7517eb725f0c6d943ad4abc4211d7
Jay Barr <jayrbarr@gmail.com>
2020-06-10 13:33:18 -0400

    Completes basic setup of Express, React, Webpack

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Cryptocurrency Charting Tool
+
+This project has been created using **webpack scaffold**, you can now run
+
+```
+npm run build
+```
+
+or
+
+```
+yarn build
+```
+
+to bundle your application
--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,22 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chart: []
+    }
+  }
+
+  render() {
+    return (
+      <div>This is working!</div>
+    )
+  }
+}
 
 
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
